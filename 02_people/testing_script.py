12ab2808738896318b7dab4fe91b32ffc29dfaae
Osama Mahmoud <142093294+osama-inv@users.noreply.github.com>
2023-12-09 18:36:16 +0200

    Update testing_script.py

--- 02_people/testing_script.py
+++ 02_people/testing_script.py
@@ -1,53 +1,101 @@
-#!/usr/bin/python3
-"""This is automated testing for this api"""
-import requests
+#include "shell.h"
 
-url = 'https://hngmus2.onrender.com/api/'
+void help_all(void);
+void help_alias(void);
+void help_cd(void);
+void help_exit(void);
+void help_help(void);
 
-#create person
-person_information = {
-    'name': 'tester1'
+/**
+ * help_all - Displays all possible builtin shellby commands.
+ */
+void help_all(void)
+{
+	char *msg = "Shellby\nThese shell commands are defined internally.\n";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "Type 'help' to see this list.\nType 'help name' to find ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "out more about the function 'name'.\n\n  alias   \t";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "alias [NAME[='VALUE'] ...]\n  cd    \tcd   ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "[DIRECTORY]\n  exit    \texit [STATUS]\n  env     \tenv";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\n  setenv  \tsetenv [VARIABLE] [VALUE]\n  unsetenv\t";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "unsetenv [VARIABLE]\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 }
-person = requests.post(url + 'peoples', json=person_information)
-print(person.status_code)
-print(f"person {person.json().get('id')} created sucessfully: {person.json()}")
-person_information = {
-    'name': 'tester2'
+
+/**
+ * help_alias - Displays information on the shellby builtin command 'alias'.
+ */
+void help_alias(void)
+{
+	char *msg = "alias: alias [NAME[='VALUE'] ...]\n\tHandles aliases.\n";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\n\talias: Prints a list of all aliases, one per line, in ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "the format NAME='VALUE'.\n\talias name [name2 ...]:prints";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " the aliases name, name2, etc. one per line, in the ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "form NAME='VALUE'.\n\talias NAME='VALUE' [...]: Defines";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " an alias for each NAME whose VALUE is given. If NAME ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "is already an alias, replace its value with VALUE.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 }
-person = requests.post(url + 'peoples', json=person_information)
-print(person.status_code)
-print(f"person {person.json().get('id')} created sucessfully: {person.json()}")
-person_information = {
-    'name': 'tester3'
+
+/**
+ * help_cd - Displays information on the shellby builtin command 'cd'.
+ */
+void help_cd(void)
+{
+	char *msg = "cd: cd [DIRECTORY]\n\tChanges the current directory of the";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " process to DIRECTORY.\n\n\tIf no argument is given, the ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "command is interpreted as cd $HOME. If the argument '-' is";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " given, the command is interpreted as cd $OLDPWD.\n\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\tThe environment variables PWD and OLDPWD are updated ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "after a change of directory.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 }
-person = requests.post(url + 'peoples', json=person_information)
-print(person.status_code)
-user_id = person.json().get('id')
-print(f"person {person.json().get('id')} created sucessfully: {person.json()}")
 
-#get all person
-person = requests.get(url + 'peoples')
-print(f"All people in the database are {person.json()}")
+/**
+ * help_exit - Displays information on the shellby builtin command 'exit'.
+ */
+void help_exit(void)
+{
+	char *msg = "exit: exit [STATUS]\n\tExits the shell.\n\n\tThe ";
 
-#get a person
-person = requests.get(url + 'people/2')
-print(f"get information about person with {person.json().get('id')} is {person.json()}")
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "STATUS argument is the integer used to exit the shell.";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " If no argument is given, the command is interpreted as";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " exit 0.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
 
-#update a person
-person = requests.put(url + 'people/1', json={'name': '1-tester'})
-print(f"The updated information about person with {person.json().get('id')} is {person.json()}")
+/**
+ * help_help - Displays information on the shellby builtin command 'help'.
+ */
+void help_help(void)
+{
+	char *msg = "help: help\n\tSee all possible Shellby builtin commands.\n";
 
-#search a person
-person = requests.get(url + 'people_search', params={'name': 'tester3'})
-print(f"search a person with name  tester3 amd they are {person.json()}")
-
-#delete a person
-person = requests.delete(url + f'people/{user_id}')
-print(f"Deleted the perdon with id {person.json().get('id')} successfully")
-
-#gethjjjjjjhg
-stats = requests.get(url + 'stats')
-print(stats.status_code)
-print(f"The database stats is {stats.json()}")
-
-
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\n      help [BUILTIN NAME]\n\tSee specific information on each ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "builtin command.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
